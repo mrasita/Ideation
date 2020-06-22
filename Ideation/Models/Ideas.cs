2fde38a464e12959b4f433aaf20da53d5517db4a
mrasita <mrakinalp@gmail.com>
2020-06-22 18:09:45 +0300

    Initial commit

---
+++ Ideation/Models/Ideas.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ideation.Models
+{
+    public class Ideas
+    {
+        public int Id { get; set; }
+
+        public string Idea { get; set; }
+
+        public Meeting Meeting { get; set; }
+
+        public User Owner { get; set; }
+
+    }
+}
