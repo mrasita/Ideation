266a8a106a52f719ac335fe78224a221034d656e
mrasita <mrakinalp@gmail.com>
2020-06-24 21:52:41 +0300

    24.06 saat 10

--- Ideation/Controllers/AccountController.cs
+++ Ideation/Controllers/AccountController.cs
@@ -6,6 +6,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Security;
+using Ideation.Models;
+
 
 namespace Ideation.Controllers
 {
@@ -49,5 +51,55 @@ namespace Ideation.Controllers
             }
             return View();
         }
+
+        public ActionResult Signup()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Signup([Bind(Include = "Id,Name,Username,Password,Role")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Users.Add(user);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(user);
+        }
+
+        // GET: Meetings/Create
+        public ActionResult CreateMeeting()
+        {
+            return View();
+        }
+
+        // POST: Meetings/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CreateMeeting ([Bind(Include = "Id,Name,Status")] Meeting meeting)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Meetings.Add(meeting);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(meeting);
+        }
+
+        public ActionResult HomePage()
+        {
+            return View();
+        }
+
+
     }
+
 }
